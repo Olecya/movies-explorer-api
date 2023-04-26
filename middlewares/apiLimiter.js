ec4bddb7c2f68016b0599bcd3f433e398d83a7bf
Olecya <OlecyaMarkina@yandex.ru>
2023-04-27 00:41:24 +0300

    v1.2

--- middlewares/apiLimiter.js
+++ middlewares/apiLimiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports = () => {
+module.exports = (req, res, next) => {
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100,
   });
+  next();
 };
